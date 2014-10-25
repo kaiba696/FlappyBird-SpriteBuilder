d0500edea32c6a5ff3772c7341bd4c650791c7fe
Kai Aichholz <kai.aichholz@gmail.com>
2014-10-24 21:16:53 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,13 +7,31 @@
 - (void)initialize
 {
     // your code here
+    character = (Character *)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
+    [self addObstacle];
+    timeSinceObstacle = 0.f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    
+    if (timeSinceObstacle > 2.0f)
+    {
+        [self addObstacle];
+        
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    [character flap];
+}
+
+
 
 @end
